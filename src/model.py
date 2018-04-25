5dbb2b2194d2e6131f035514360079a177c072ae
Alex Xu <a.i.xu@me.com>
2018-04-25 15:08:38 -0400

    Init commit 2018/04/25

---
+++ src/model.py
@@ -0,0 +1,18 @@
+import mynet
+
+def gen(input_shape,model,nclasses=6,p_out=None):
+    if model=='mynet':
+        if not nclasses:
+            return mynet.build_rmse(input_shape)
+        else:
+            return mynet.build(input_shape,nclasses)
+    if model=='mynet_card':
+        return mynet_card.build(input_shape,nclasses)    
+    if model=='resnet50':
+        return resnet50.build(input_shape,nclasses)
+    if model=='unet':
+        return unet.build(input_shape,nclasses)
+    if model=='classnet':
+        return classnet.build(input_shape,nclasses)
+    if model=='carnet':
+        return carnet.build(input_shape,nclasses)
